be777e0aa6494e714eab79ea789fc5f31bb6cc23
Cristiane Nazareth <cristianenazareth24@gmail.com>
2023-05-31 19:05:01 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-
 import React from "react";
+
 import Body from "./components/Body/Body";
-import Main from "./components/Main/Main";
-import Form from "./components/Form/Form"
+// import Main from "./components/Main/Main";
+// import Form from "./components/Form/Form";
 
 const App = () => {
   return (
     
     <React.Fragment>
-      <Body>
-        <Main>
-          <Form>
-          </Form>
-        </Main>
-      </Body>
+      
+      <Body></Body>
 
       <h3>ola, mundo</h3>
     </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Body from './components/Body/Body';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
   
   <React.Fragment>
     
